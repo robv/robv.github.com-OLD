d5b8ab0159a8b84c0d03ffaebde6fd5179a063a9
robv <thisisrobv@gmail.com>
2011-06-05 00:33:55 -0700

    Initial commit.

---
+++ _site/js/common.js
@@ -0,0 +1,38 @@
+var Site;
+
+(function($) {
+	Site = {
+
+		// This vars should be set in <head> server-side
+		config: {
+			base_url: '',
+			site_url: ''
+		},
+	
+		// This method is called on every page
+		init: function() {
+			
+			// On Dom Ready
+			$(function() {
+				
+				WebFontConfig = {
+          google: { families: [ 'Molengo', 'Merriweather:light,regular,bold,900' ] }
+        };
+        
+        (function() {
+          var wf = document.createElement('script');
+          wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+              '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+          wf.type = 'text/javascript';
+          wf.async = 'true';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(wf, s);
+        })();
+        
+			});
+
+		}
+	};
+})(jQuery);
+
+Site.init();
